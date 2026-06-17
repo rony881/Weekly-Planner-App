1e21f1ede374324e525e3bbe5326b52f0111ca1e
Rony <ronybiswasinnovative@gmail.com>
2026-06-18 00:32:40 +0600

    Added Layout

--- main.py
+++ main.py
@@ -23,25 +23,63 @@ class Mainwindow(QMainWindow):
         self.side_bar.setFixedWidth(250)
         self.side_bar.setStyleSheet(
             """
-            background-color: #191919;
+            background-color: #161618;
+            border-right: 1px solid #2e2e2e;
             """
         )
-
         self.central_panel = QFrame()
-        self.central_panel.setStyleSheet(
+
+        self.weekly_calender = QFrame()
+        self.weekly_calender.setFixedHeight(100)
+        self.weekly_calender.setStyleSheet(
             """
-            background-color: #191919;
+            background-color: #161618;
+            border: 1px solid #2e2e2e;
+            border-radius: 7px;
+            """
+        )
+        self.task_area = QFrame()
+        self.task_area.setStyleSheet(
+            """
+            background-color: #161618;
+            border: 1px solid #2e2e2e;
+            border-radius: 7px;
+            """
+        )
+        self.habit_tracker_area = QFrame()
+        self.habit_tracker_area.setStyleSheet(
+            """
+            background-color: #161618;
+            border: 1px solid #2e2e2e;
+            border-radius: 7px;
+            """
+        )
+        self.progress_area = QFrame()
+        self.progress_area.setFixedHeight(100)
+        self.progress_area.setStyleSheet(
+            """
+            background-color: #161618;
+            border: 1px solid #2e2e2e;
+            border-radius: 7px;
             """
         )
 
         self._setup_layout()
     def _setup_layout(self):
-        self.main_layout = QHBoxLayout(self.central_widget)
+        
+        # Central Layout
+        self.central_layout = QHBoxLayout(self.central_widget)
+        self.central_layout.setContentsMargins(0, 0, 0, 0)
+        self.central_layout.addWidget(self.side_bar)
+        self.central_layout.addWidget(self.central_panel)
+
         self.side_layout = QVBoxLayout(self.side_bar)
 
-        self.main_layout.addWidget(self.side_bar)
-        self.main_layout.addWidget(self.central_panel)
-
+        self.panel_layout = QVBoxLayout(self.central_panel)
+        self.panel_layout.addWidget(self.weekly_calender)
+        self.panel_layout.addWidget(self.task_area)
+        self.panel_layout.addWidget(self.habit_tracker_area)
+        self.panel_layout.addWidget(self.progress_area)
 
 
 
