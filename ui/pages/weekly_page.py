78a8f308624ad2dd2a7c4db2cfe5f484f09825b9
Rony <ronybiswasinnovative@gmail.com>
2026-06-23 12:35:55 +0600

    Updated Weekly Tab and Card

--- ui/pages/weekly_page.py
+++ ui/pages/weekly_page.py
@@ -1,53 +1,88 @@
 from sched import scheduler
 
 from PyQt6.QtWidgets import (
-    QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton,
-    QScrollArea, QFrame, QGridLayout, QInputDialog,QTabWidget
+    QWidget, QVBoxLayout,QTabWidget
 )
 from PyQt6.QtCore import Qt
 from ui.widgets.card import TaskCard
-from qfluentwidgets import SimpleCardWidget,TitleLabel, TabBar
+from qfluentwidgets import TitleLabel,SmoothScrollArea
 
-schedule = {
-    "Morning" : ["Reading", "Drawing", "Meditation"],
-    "AfterNoon" : ["Exercise", "Drawing", "Meditation"],
-    "Knight" : ["Reading", "Leg Day", "Meditation"]
-}
 
-week = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"]
+WEEKLY_SCHEDULE = {
+    "Sat" : {
+        "Morning" : ["Reading", "Drawing", "Meditation"],
+        "AfterNoon" : ["Exercise", "Drawing", "Meditation"],
+        "Night" : ["Reading", "Leg Day", "Meditation"]
+    },
+    "Sun" : {
+        "Morning" : ["Reading", "Drawing", "Meditation"],
+        "AfterNoon" : ["Exercise", "Drawing", "Meditation"],
+        "Night" : ["Reading", "Leg Day", "Meditation"]
+    },
+    "Mon" : {
+        "Morning" : ["Reading", "Drawing", "Meditation"],
+        "AfterNoon" : ["Exercise", "Drawing", "Meditation"],
+        "Night" : ["Reading", "Leg Day", "Meditation"]
+    },
+    "Tue" : {
+        "Morning" : ["Reading", "Drawing", "Meditation"],
+        "AfterNoon" : ["Exercise", "Drawing", "Meditation"],
+        "Night" : ["Reading", "Leg Day", "Meditation"]
+    },
+    "Wed" : {
+        "Morning" : ["Reading", "Drawing", "Meditation"],
+        "AfterNoon" : ["Exercise", "Drawing", "Meditation"],
+        "Night" : ["Reading", "Leg Day", "Meditation"]
+    },
+    "Thu" : {
+        "Morning" : ["Reading", "Drawing", "Meditation"],
+        "AfterNoon" : ["Exercise", "Drawing", "Meditation"],
+        "Night" : ["Reading", "Leg Day", "Meditation"]
+    },
+    "Fri" : {
+        "Morning" : ["Reading", "Drawing", "Meditation"],
+        "AfterNoon" : ["Exercise", "Drawing", "Meditation"],
+        "Night" : ["Reading", "Leg Day", "Meditation"]
+    }
+} 
 
 class WeeklyTab(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.layout = QVBoxLayout(self)
+        layout = QVBoxLayout(self)
 
         # ================
         # Header / Title
         # ================
         title = TitleLabel("Weekly Schedule")
-        self.layout.addWidget(title)
+        layout.addWidget(title)
 
         # ================
         # Tabbar
         #================
         tabs = QTabWidget()
-        for day in week:
+        for day,schedule in WEEKLY_SCHEDULE.items():
             tabs.addTab(DayWidget(schedule),day)
 
-        self.layout.addWidget(tabs)
+        layout.addWidget(tabs)
 
-class DayWidget(QWidget):
+class DayWidget(SmoothScrollArea):
     def __init__(self, routine):
         super().__init__()
 
-        self.layout = QVBoxLayout(self)
+        self.setWidgetResizable(True)
+
+        mainLayout = QVBoxLayout(self)
+
+        # Content widget 
+        content = QWidget()
+        contentLayout = QVBoxLayout(content)
+        self.setWidget(content)
 
         for time, tasks in routine.items():
             for task in tasks:
                 card = TaskCard(task, time)
-                self.layout.addWidget(card)
+                contentLayout.addWidget(card)
 
-
-        
-
+        contentLayout.addStretch()
--- ui/widgets/card.py
+++ ui/widgets/card.py
@@ -1,31 +1,29 @@
 from PyQt6.QtWidgets import (
-    QFrame, QLabel, QPushButton,
-    QVBoxLayout, QHBoxLayout, QWidget, QSizePolicy
+    QVBoxLayout, QWidget, QSizePolicy,QHBoxLayout
 )
-from PyQt6.QtCore import Qt
 from qfluentwidgets import CardWidget,SubtitleLabel,PushButton,BodyLabel
+
 
 class TaskCard(CardWidget):
     def __init__(self, task, time):
         super().__init__()
 
+        self.setFixedHeight(100)
+
         main_layout = QVBoxLayout(self)
         main_layout.setContentsMargins(10, 10, 10, 10)
-        main_layout.setSpacing(8)
 
-        # Title Label
-        time_label = SubtitleLabel(time)
-        # Task
-        task_name = BodyLabel(task)
-        # Menu Button ⋮
-        menu_button = PushButton("Complete")
+        self.time_label = SubtitleLabel(time)
+        self.task_label = BodyLabel(task)
+        self.complete_button = PushButton("Complete")
 
-        # # Top Area
-        # top_area = QLabel()
-        # top_layout = QHBoxLayout(top_area)
+        main_layout.addWidget(self.time_label)
+        main_layout.addWidget(self.task_label)
 
-        # top_layout.addWidget
+        main_layout.addStretch()
 
-        main_layout.addWidget(time_label)
-        main_layout.addWidget(task_name)
-        main_layout.addWidget(menu_button)
+        button_layout = QHBoxLayout()
+        button_layout.addStretch()
+        button_layout.addWidget(self.complete_button)
+
+        main_layout.addLayout(button_layout)
